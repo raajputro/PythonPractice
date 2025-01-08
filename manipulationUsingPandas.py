0c65055f73b423b24ff11350532bc8896a504be2
raajputro <mahbuburrahmannajib@gmail.com>
2025-01-08 16:55:40 +0600

    additional dataframe writing

--- manipulationUsingPandas.py
+++ manipulationUsingPandas.py
@@ -20,11 +20,33 @@ def create_new_sheets(input_file, input_sheet_name, output_file, output_sheet_na
     except Exception as e:
         print(f"An error occurred: {e}")
 
-def create_data_frame(input_file, input_sheet_name, output_file, output_sheet_name, row_indices, column_indices):
+def create_data_frame(input_file, input_sheet_name, row_indices, column_indices):
     try:
         df = pd.read_excel(input_file, sheet_name=input_sheet_name)
         selected_data = df.iloc[row_indices, column_indices]
         return pd.DataFrame(selected_data, columns=df.columns[column_indices])
+    except Exception as e:
+        print(f"An error occurred: {e}")
+
+def create_transposed_frame(input_file, input_sheet_name, row_indices, column_indices):
+    try:
+        df = pd.read_excel(input_file, sheet_name=input_sheet_name)
+        selected_data = df.iloc[row_indices, column_indices]
+        df1= pd.DataFrame(selected_data, columns=df.columns[column_indices])
+        return df1.T
+    except Exception as e:
+        print(f"An error occurred: {e}")
+
+def create_transposed_frame_from_df(dataFrame):
+    try:
+        return dataFrame.T
+    except Exception as e:
+        print(f"An error occurred: {e}")
+
+def write_to_output_file(df, output_file, output_sheet_name):
+    try:
+        with pd.ExcelWriter(output_file) as writer:
+            df.to_excel(writer, sheet_name=output_sheet_name, index=False)
     except Exception as e:
         print(f"An error occurred: {e}")
 
@@ -81,24 +103,33 @@ o_s_name = 'Scrapped_Sheet'
 # else:
 #     print(f"'{c_name}' not found in the Worksheet: {i_s_name}.")
 #
-# c_names = ['Id', 'Name', 'Email', 'CompanyCategoryId', 'ShortName']
-# r_values = ['BFS Chicken', 'MZ Resort', 'Northern Bank Limited', 'Indian Spicy King', 'The Food Factory']
-# c_indices = []
-# r_indices = []
-#
-# for c_name in c_names:
-#     c_indices.append(get_column_index(i_file,i_s_name,c_name))
-#
-# for r_value in r_values:
-#     r_indices.append(get_row_index(i_file,i_s_name,r_value,c_names[1]))
-#
+c_names = ['Id', 'Name', 'Email', 'CompanyCategoryId', 'ShortName']
+r_values = ['BFS Chicken', 'MZ Resort', 'Northern Bank Limited', 'Indian Spicy King', 'The Food Factory']
+c_indices = []
+r_indices = []
+
+for c_name in c_names:
+    c_indices.append(get_column_index(i_file,i_s_name,c_name))
+
+for r_value in r_values:
+    r_indices.append(get_row_index(i_file,i_s_name,r_value,c_names[1]))
+
 # print(c_indices)
 # print('\n')
 # print(r_indices)
 
 #create_new_sheets(i_file, i_s_name, o_file,o_s_name, r_indices, c_indices)
 
-s_value = 'Bunkers'
-s_column = 'Name'
-d_rows = find_duplicate_rows_in_column(i_file,i_s_name,s_column)
-print(d_rows)
+# s_value = 'Bunkers'
+# s_column = 'Name'
+# d_rows = find_duplicate_rows_in_column(i_file,i_s_name,s_column)
+# print(d_rows)
+
+# d = {'col1':[1,2], 'col2':[3,4], 'col3':[5,6]}
+# ddf = pd.DataFrame(data=d)
+# print(ddf)
+
+t_df = create_data_frame(input_file=i_file, input_sheet_name=i_s_name, row_indices=r_indices, column_indices=c_indices)
+write_to_output_file(df=t_df, output_file=o_file, output_sheet_name="NormalData")
+t_df1 = create_transposed_frame_from_df(t_df)
+write_to_output_file(df=t_df1, output_file=o_file, output_sheet_name="TransposedData")
