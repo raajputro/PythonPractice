4c88c7b3000125539a8d1b008803bf6f0aad8fea
raajputro <34886621+raajputro@users.noreply.github.com>
2025-01-11 01:30:01 +0600

    redefine gitignore

--- summitDataManipulation_2.py
+++ summitDataManipulation_2.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import os
-from numpy import *
+import numpy as np
 
 # Function to plot data in to excel sheet
 def write_to_output_file(df, output_file, output_sheet_name):
@@ -8,13 +8,21 @@ def write_to_output_file(df, output_file, output_sheet_name):
         with pd.ExcelWriter(output_file) as writer:
             df.to_excel(writer, sheet_name=output_sheet_name)#, index=False)
     except Exception as e:
-        print(f"An error occurred: {e}")
+        print(f"From def: write_to_output_file -> An error occurred: {e}")
 
 def create_transposed_frame_from_df(dataFrame):
     try:
         return dataFrame.T
     except Exception as e:
-        print(f"An error occurred: {e}")
+        print(f"From def: create_transposed_frame_from_df -> An error occurred: {e}")
+
+def convert_list_to_dataframe_write_to_file(givenlist, givensheet, givenfile):
+    try:
+        df = pd.DataFrame(givenlist)
+        return write_to_output_file(df=df,output_file=givenfile,output_sheet_name=givensheet)
+    except Exception as e:
+        print(f"From def: convert_list_to_dataframe_write_to_file -> An error occurred: {e}")
+        return
 
 # Parameters
 execution_directory = os.getcwd()
@@ -42,7 +50,6 @@ def return_sliced_dataFrame(input_data_frame, filter_column, filter_value, colum
         print(f"An error occurred {e}")
         return -1
 
-
 def return_selected_row_count(input_df,column_name,row_value):
     try:
         count = input_df[input_df[column_name] == row_value].shape[0]
@@ -58,38 +65,70 @@ def return_sliced_data_frame(input_df, column_name, row_value):
         print(f"An error occurred {e}")
         return -1
 
+
+
 t_df = get_all_data(input_file=i_file, input_sheet_name=i_s_name)
 
 agent_names = t_df['Agent Name'].unique()
 month_names = t_df['Month'].unique()
-
+# print(month_names)
+# dec_month = ['Dec']
+# month_names = np.setdiff1d(month_names,dec_month)
+# print(month_names)
+#month_names.remove('Dec')
+#print(month_names)
+#
 sliced_t_df_to_agent = return_sliced_dataFrame(input_data_frame=t_df,filter_column='Agent Name',filter_value=agent_names[0], column_names=c_nam)
 
 month_data = {}
+month_data_ratio = {}
 for month_name in month_names:
-    t_df_1 = return_sliced_data_frame(sliced_t_df_to_agent,'Month',month_name)
-    month_count = {}
-    month_row_count = {}
-    for i in range(1, len(c_nam)):
-        cname = c_nam[i]
-        month_row_count[cname, month_name] = len(t_df_1)
-        month_count[cname] = return_selected_row_count(input_df=t_df_1, column_name=cname, row_value='Pass')
-    month_data[month_name] = month_count
-    print(month_row_count)
+    if month_name != 'Dec':
+        t_df_1 = return_sliced_data_frame(sliced_t_df_to_agent,'Month',month_name)
+        month_count = {}
+        month_ratio = {}
+        for i in range(1, len(c_nam)):
+            cname = c_nam[i]
+            month_count[cname] = return_selected_row_count(input_df=t_df_1, column_name=cname, row_value='Pass')
+            #print(f"CNAME: {cname} and MOUNTH_COUNT: {month_count[cname]}")
+            month_ratio[cname] = round((month_count[cname] * 100) / len(t_df_1))
+        month_data[month_name] = month_count
+        month_data_ratio[month_name] = month_ratio
+
 #print(month_data)
-
-
-# exp_df = pd.DataFrame(month_data)
-# exp_df_2 = exp_df.T
-# exp_df_3 = exp_df_2.T
-# #print(exp_df_3)
-# write_to_output_file(exp_df_3,o_file,agent_names[0])
+t_df_2 = pd.DataFrame(month_data_ratio)
+#print(t_df_2)
+#print(month_data)
+# print(f"Length is: {len(month_data_ratio)}")
 #
-# for mData in month_data:
-#     mDataValue = month_data[mData]
-#     for i in range(1, len(c_nam)):
-#         mDataValue_AL = mDataValue[c_nam[i]]
-#         #print(mData,'|', c_nam[i],'|', mDataValue_AL)
-#         # print(mData)
-#         # print(c_nam[i])
-#         print(mDataValue_AL)
+# cname_avg = 0
+# for mname in month_names:
+#     if mname != 'Dec':
+#         cname_avg += month_data_ratio[mname]['Active Listening']
+#         print(month_data_ratio[mname]['Active Listening'])
+# cname_avg = round(cname_avg/11)
+#print(f"Avg: {cname_avg}")
+t_df_2['avg'] = round(t_df_2.iloc[:,1:len(t_df_2.columns)].mean(axis=1))
+print(t_df_2)
+t_df_2.loc[len(t_df_2)] = ['row avg', round(t_df_2.iloc[:,2:len(t_df_2.columns)].mean())]
+
+#print(round(t_df_2.iloc[:,1:len(t_df_2.columns)].mean()))
+print(t_df_2)
+
+
+#convert_list_to_dataframe_write_to_file(givenlist=month_data_ratio, givensheet=agent_names[0], givenfile=o_file)
+#
+# # exp_df = pd.DataFrame(month_data)
+# # exp_df_2 = exp_df.T
+# # exp_df_3 = exp_df_2.T
+# # #print(exp_df_3)
+# # write_to_output_file(exp_df_3,o_file,agent_names[0])
+# #
+# # for mData in month_data:
+# #     mDataValue = month_data[mData]
+# #     for i in range(1, len(c_nam)):
+# #         mDataValue_AL = mDataValue[c_nam[i]]
+# #         #print(mData,'|', c_nam[i],'|', mDataValue_AL)
+# #         # print(mData)
+# #         # print(c_nam[i])
+# #         print(mDataValue_AL)
