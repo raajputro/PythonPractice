c642a3c31dc0014b7bb93627009cf4ebe37b3d3a
raajputro <mahbuburrahmannajib@gmail.com>
2025-01-15 16:52:11 +0600

    master done

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 1. Install python
 2. Install openpyxl: pip install openpyxl
 3. Install pandas: pip install pandas
+
+
+
+After complete installation, execute the file,
+either from the IDE-> run button
+or, open cmd and write: python summitDataManipulation.py
--- summitDataManipulation.py
+++ summitDataManipulation.py
@@ -1,28 +1,62 @@
-import pandas as pd
 import os
-#ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
-ROOT_DIR = os.getcwd()
-print(ROOT_DIR)
+import pandas as pd
+from openpyxl.reader.excel import load_workbook
+from openpyxl.utils.dataframe import dataframe_to_rows
+from openpyxl.workbook import Workbook
+from openpyxl.styles import Font, Alignment
 
-def write_to_output_file(df, output_file, output_sheet_name):
+
+# Function to plot data in to excel sheet
+def write_multi_dfs_to_file(output_file, output_sheet, dataframes):
     try:
-        with pd.ExcelWriter(output_file) as writer:
-            df.to_excel(writer, sheet_name=output_sheet_name)#, index=False)
-    except Exception as e:
-        print(f"An error occurred: {e}")
+        workbook = load_workbook(output_file)
+        if output_sheet in workbook.sheetnames:
+            workbook.remove(workbook[output_sheet])
+        worksheet = workbook.create_sheet(output_sheet)
+    except FileNotFoundError:
+        workbook = Workbook()
+        worksheet = workbook.active
+        worksheet.title = output_sheet
 
-execution_directory = os.getcwd()
-i_file = execution_directory + "\\Data\\Profiling Master- QC.xlsx"
-i_s_name = 'Main Data'
-o_file = execution_directory + "\\OutputFile2.xlsx"
-o_s_name = 'Scrapped_Sheet'
+    start_row = 1
+    for i in range(len(dataframes)):
+        df = dataframes[i]
+        start_col = 13 if (i > 0 and i % 2 == 0) else 1
+        index = False if (i > 0 and i % 2 == 0) else True
+        header = False if (i > 0 and i % 2 == 0) else True
+        #print(f"Dataframe index: {i}, Length: {len(df)}, start row: {start_row}, start col: {start_col}")
+        for r_idx, row in enumerate(dataframe_to_rows(df, index=index, header=header), start=start_row):
+            for c_idx, value in enumerate(row, start=start_col):
+                cell = worksheet.cell(row=r_idx, column=c_idx, value=value)
+                # Apply bold font to the header row
+                if r_idx == start_row and (i==0 or (i>0 and i%2!=0)):
+                    cell.font = Font(bold=True)
+                    cell.alignment = Alignment(horizontal='center', vertical='center')
+        start_row += len(df) + (3 if i == 0 else 0) + (3 if i % 2 == 0 else 2)
+    workbook.save(output_file)
 
-c_nam = ['Month', 'Active Listening', 'Verbal Excellence', 'Courteous Approach', 'Identification and Action for Resolution', 'Correct & Complete Information For Resolution (CCIR)', 'Avoid Rude/Unprofessional Behavior/Approach (ARU)','Ownership & Proctiveness (OP)']
-
-def return_selected_columns_of_selected_row(input_file, input_sheet_name ,filter_column,filter_value, c_names):
+# Custom functions to slice main data to specific data
+####################################################################################################################
+def get_all_data(input_file, input_sheet_name):
     try:
         df = pd.read_excel(input_file, sheet_name=input_sheet_name)
-        return df[df[filter_column]==filter_value][c_names]
+        return df
+    except Exception as e:
+        print(f"An error occurred {e}")
+        return -1
+
+def return_sliced_dataframe(input_data_frame, filter_column, filter_value, column_names):
+    try:
+        df = input_data_frame
+        return df[df[filter_column]==filter_value][column_names]
+    except Exception as e:
+        print(f"An error occurred {e}")
+        return -1
+
+def return_selected_row_count(input_df,column_name,row_value):
+    try:
+        count = input_df[input_df[column_name] == row_value].shape[0]
+        return count
     except Exception as e:
         print(f"An error occurred {e}")
         return -1
@@ -34,53 +68,94 @@ def return_sliced_data_frame(input_df, column_name, row_value):
         print(f"An error occurred {e}")
         return -1
 
-def return_selected_row_count(input_df,column_name,row_value):
-    try:
-        #return (input_df[column_name]==row_value).sum()
-        count = input_df[input_df[column_name] == row_value].shape[0]
-        return count
-    except Exception as e:
-        print(f"An error occurred {e}")
-        return -1
+def slice_month_data_ratio(month_data_ratio, month_names, attributes_name):
+    sliced_month_data = {}
+    for mnam in month_names:
+        if mnam != 'Dec':
+            sm_data = {}
+            for anam in attributes_name:
+                val = month_data_ratio[mnam]
+                val2 = val[anam]
+                sm_data[anam] = val2
+            sliced_month_data[mnam] = sm_data
+    test_df = pd.DataFrame(sliced_month_data)
+    test_df['avg'] = round(test_df.iloc[:, 1:len(test_df.columns)].mean(axis=1))
+    avg_val1 = round(float(test_df['avg'].mean()),2)
+    avg_val2 = round(float(avg_val1/20),2)
+    avg_df = pd.DataFrame({ 'values': [avg_val1, avg_val2] })
+    return [test_df, avg_df]
 
-t_df = return_selected_columns_of_selected_row(input_file=i_file,input_sheet_name=i_s_name,filter_column='Agent Name',filter_value='Shusmoy Kundu',c_names=c_nam)
-#print(t_df)
-#write_to_output_file(df=t_df,output_file=o_file,output_sheet_name=o_s_name)
+####################################################################################################################
+####################################################################################################################
+####################################################################################################################
+# Parameters
+execution_directory = os.getcwd()
+i_file = execution_directory + "\\Data\\Profiling Master.xlsx"
+i_s_name = 'Main Data'
+o_file = execution_directory + "\\Data\\Profiling Master - Output.xlsx"
+o_s_name = 'Scrapped_Sheet'
+c_nam = ['Month','Active Listening', 'Verbal Excellence', 'Courteous Approach',
+         'Identification and Action for Resolution', 'Correct & Complete Information For Resolution (CCIR)',
+         'Avoid Rude/Unprofessional Behavior/Approach (ARU)','Ownership & Proctiveness (OP)']
 
+t_df = get_all_data(input_file=i_file, input_sheet_name=i_s_name)
+
+agent_names = t_df['Agent Name'].unique()
 month_names = t_df['Month'].unique()
-#print(month_names)
 
-month_data = {}
-for month_name in month_names:
-    t_df_1 = return_sliced_data_frame(t_df,'Month',month_name)
-    #write_to_output_file(t_df_1,o_file,output_sheet_name=month_name)
-    #print(t_df_1)
-    month_count = {}
-    for i in range(1, len(c_nam)):
-        cname = c_nam[i]
-        #print(cname)
-        month_count[cname] = return_selected_row_count(input_df=t_df_1, column_name=cname, row_value='Pass')
-        month_data[month_name] = month_count
-    print(month_data)
-#print(month_data)
+####################################################################################################################
+for agent_name in agent_names:
+#agent_name = agent_names[0]
+    sliced_t_df_to_agent = return_sliced_dataframe(input_data_frame=t_df, filter_column='Agent Name',
+                                                   filter_value=agent_name, column_names=c_nam)
+    month_data = {}
+    month_data_ratio = {}
+    for month_name in month_names:
+        if month_name != 'Dec':
+            t_df_1 = return_sliced_data_frame(sliced_t_df_to_agent,'Month',month_name)
+            td_len = len(t_df_1)
+            if td_len == 0:
+                td_len = 1
+            month_count = {}
+            month_ratio = {}
+            for i in range(1, len(c_nam)):
+                cname = c_nam[i]
+                month_count[cname] = return_selected_row_count(input_df=t_df_1, column_name=cname, row_value='Pass')
+                month_ratio[cname] = round((month_count[cname] * 100) / td_len)
+            month_data[month_name] = month_count
+            month_data_ratio[month_name] = month_ratio
 
+    ################### DataFrame for Pass/Fail #######################################
+    t_df_11 = pd.DataFrame(month_data)
+    ################### DataFrame having ratio values ################################# to test data
+    t_df_2 = pd.DataFrame(month_data_ratio)
+    t_df_2['avg'] = round(t_df_2.iloc[:,1:len(t_df_2.columns)].mean(axis=1))
 
-#write_to_output_file(df=t_df,output_file=o_file,output_sheet_name=o_s_name)
+    ####################################################################################################################
+    #creating data frames array, according to following attribute groups
+    ####################################################################################################################
+    com_cnames = ['Verbal Excellence', 'Avoid Rude/Unprofessional Behavior/Approach (ARU)']
+    emp_cnames = ['Courteous Approach', 'Active Listening']
+    bus_cnames = ['Correct & Complete Information For Resolution (CCIR)', 'Identification and Action for Resolution']
+    acc_cnames = ['Ownership & Proctiveness (OP)']
+    dfs_array = [t_df_11]
+    smdr = slice_month_data_ratio(month_data_ratio,month_names,com_cnames)
+    for x in smdr:
+        dfs_array.append(x)
 
-# month_data = {}
-# for month_name in month_names:
-#     t_df_1 = return_sliced_data_frame(t_df,'Month',month_name)
-#     #write_to_output_file(df=t_df_1,output_file=o_file,output_sheet_name=o_s_name)
-#
-#     month_count = {}
-#     # for cname in c_nam:
-#     #     if cname == 'Month':
-#     #         month_count[cname]= return_selected_row_count(input_df=t_df_1,column_name=cname,row_value='Jan')
-#     #     else:
-#     #         month_count[cname] = return_selected_row_count(input_df=t_df_1, column_name=cname, row_value='Pass')
-#     for i in range(1, len(c_nam)):
-#         cname = c_nam[i]
-#         month_count[cname] = return_selected_row_count(input_df=t_df_1, column_name=cname, row_value='Pass')
-#     month_data[month_name] = month_count
-# print(month_data)
-# #write_to_output_file(df=t_df,output_file=o_file,output_sheet_name=o_s_name)
+    smdr = slice_month_data_ratio(month_data_ratio,month_names,emp_cnames)
+    for x in smdr:
+        dfs_array.append(x)
+
+    smdr = slice_month_data_ratio(month_data_ratio,month_names,bus_cnames)
+    for x in smdr:
+        dfs_array.append(x)
+
+    smdr = slice_month_data_ratio(month_data_ratio,month_names,acc_cnames)
+    for x in smdr:
+        dfs_array.append(x)
+    ####################################################################################################################
+    # finally, write all data frames to work sheet
+    ####################################################################################################################
+    agent_name = agent_name[:30]
+    write_multi_dfs_to_file(output_file=o_file, output_sheet=agent_name, dataframes=dfs_array)
